63f3d94550199013a601ba569ded8149e7315e1d
produser <job29046@zcrcd.com>
2022-06-07 06:56:49 +0000

    file was updated from testinium web-ide : "src/test/java/com/testinium/page/HomePage.java"

--- src/test/java/com/testinium/page/HomePage.java
+++ src/test/java/com/testinium/page/HomePage.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+ıjjhkkj
 
 public class HomePage {
     Logger log= LogManager.getLogger(Methods.class);
@@ -15,7 +16,7 @@ public class HomePage {
     public void home(){
         log.info("Elementin hazır olması bekleniyor");
         methods.waitUntilPresence(By.id("nav-global-location-popover-link"));
-      sasad  Assert.assertTrue(methods.waitUntilElementAppear(By.id("nav-global-location-popover-link")));
+    Assert.assertTrue(methods.waitUntilElementAppear(By.id("nav-global-location-popover-link")));
 
     }
 }
